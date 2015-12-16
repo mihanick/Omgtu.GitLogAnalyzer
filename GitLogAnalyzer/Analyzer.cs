dca8fdecd03a102a804a6ed1be4440bf8e586435
mihanick <mihanick@gmail.com>
2015-12-16 20:26:01 +0600

    Реализовать класс создающий LogEntry из файла

--- GitLogAnalyzer/Analyzer.cs
+++ GitLogAnalyzer/Analyzer.cs
@@ -14,6 +14,30 @@ namespace Omgtu.GitLog
             public string Author { get; set; }
             public string Message { get; set; }
             public string Date { get; set; }
+
+            public LogEntry()
+            {
+                this.Author = string.Empty;
+                this.Message = string.Empty;
+                this.Date = string.Empty;
+            }
+
+            public override string ToString()
+            {
+                return this.Date + this.Author + this.Message;
+            }
+
+            public bool IsValid()
+            {
+                if (string.IsNullOrEmpty(this.Author))
+                    return false;
+                if (string.IsNullOrEmpty(this.Date))
+                    return false;
+                if (string.IsNullOrEmpty(this.Message))
+                    return false;
+
+                return true;
+            }
         }
 
         public List<LogEntry> Entries { get; set; }
@@ -25,11 +49,53 @@ namespace Omgtu.GitLog
 
         public List<LogEntry> Parse(List<string> strings)
         {
-            // TODO: Реалзовать
-            return new List<LogEntry>();
+            List<LogEntry> result = new List<LogEntry>();
 
+            LogEntry logEntry = new LogEntry();
+
+            foreach (string line in strings)
+            {
+                string parsedLine = ValueFromString("Commit:", line);
+                if (!string.IsNullOrEmpty(parsedLine))
+                {
+                    if (logEntry.IsValid())
+                        result.Add(logEntry);
+
+                    logEntry = new LogEntry();
+                }
+                else
+                {
+                    parsedLine = ValueFromString("Author:", line);
+                    if (!string.IsNullOrEmpty(parsedLine))
+                    {
+                        logEntry.Author = parsedLine;
+                    }
+                    else
+                    {
+                        parsedLine = ValueFromString("Date:", line);
+                        if (!string.IsNullOrEmpty(parsedLine))
+                        {
+                            logEntry.Date = parsedLine;
+                        }
+                        else
+                        {
+                                logEntry.Message+=line;
+                        }
+                    }
+                }
+            }
+
+            return result;
         }
 
+        internal string ValueFromString(string lineName, string inputLine)
+        {
+            if (!inputLine.StartsWith(lineName))
+                return string.Empty;
+
+            string result = inputLine.Substring(lineName.Length-1,inputLine.Length - lineName.Length);
+            return result;
+        }
 
         internal List<string> ReadFile(string sourcePath)
         {
@@ -53,3 +119,4 @@ namespace Omgtu.GitLog
         }
     }
 }
+
