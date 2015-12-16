04b4642aa537a876c9ad1cbe2ea25d82096b9dc5
mihanick <mihanick@gmail.com>
2015-12-16 19:47:04 +0600

    Добавить тесты на непустые списки и количество вхождений

---
+++ GitLogProgram/TestAnalyzer/TestAnalyzer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Omgtu.GitLog
+{
+    [TestClass]
+    public class TestAnalyzer
+    {
+        [TestMethod]
+        public void TestReadFileReturnsNotEmpty()
+        {
+            string fileName = "log.txt";
+            Analyzer analyzer = new Analyzer(fileName);
+
+            Assert.AreNotEqual(0, analyzer.Entries.Count);
+        }
+
+        [TestMethod]
+        public void TestNumberOfEntries()
+        {
+            string fileName = "log.txt";
+            Analyzer analyzer = new Analyzer(fileName);
+
+            // Count number of lines starting with "Commit"
+            string line;
+            int numCommitsExpected = 0;
+            using (StreamReader file = new StreamReader(fileName))
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (line.StartsWith(""))
+                        numCommitsExpected++;
+                }
+            }
+
+            Assert.Equals(numCommitsExpected, analyzer.Entries.Count);
+        }
+    }
+}
+
