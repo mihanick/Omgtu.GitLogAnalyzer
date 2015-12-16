8917676c5dce9e639540eac1fcc6b7badb4ece91
mihanick <mihanick@gmail.com>
2015-12-16 22:01:14 +0600

    Реализовать программу использующую класс анализатора
    Программа получает имя входного и выходного файла в качестве параметров
    командной строки.

--- GitLogProgram/GitLogProgram/Program.cs
+++ GitLogProgram/GitLogProgram/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,38 @@ namespace Omgtu.GitLog
     {
         static void Main(string[] args)
         {
+            string inputFile = @"C:\github\Omgtu.GitLogAnalyzer\GitLogProgram\TestAnalyzer\log.txt";
+            string outputFile = @"C:\github\Omgtu.GitLogAnalyzer\GitLogProgram\TestAnalyzer\output.txt";
+
+            if (args.Length == 2)
+            {
+                inputFile = args[0];
+                outputFile = args[1];
+            }
+
+            Analyzer analyzer = new Analyzer(inputFile);
+
+            string author = string.Empty;
+
+            using (StreamWriter sw = new StreamWriter(outputFile))
+            {
+                foreach (Analyzer.LogEntry logEntry in analyzer.Entries)
+                {
+                    if (logEntry.Author != author)
+                    {
+                        Console.WriteLine(logEntry.Author);
+                        sw.WriteLine(logEntry.Author);
+
+                        author = logEntry.Author;
+                    }
+                    string entry = string.Format("\t{0} {1}", logEntry.Date, logEntry.Message);
+
+                    Console.WriteLine(entry);
+                    sw.WriteLine(entry);
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
